b87ae139e8227d967d25b3cff2dbadc9e6237f5c
Shahzaib-A <54896758+Shahzaib-A@users.noreply.github.com>
2019-11-11 09:32:01 -0500

    hn

--- Projects.js
+++ Projects.js
@@ -3,6 +3,19 @@ function century(){
     alert("the century is "+ year)
 }
 
-function four_ten(){
-    
+function tenfour(){
+    for(let i = 0; i < 101; i++){
+        if(i % 10 == 0 && i % 4 ==0){
+            console.log("tenfour");
+        }
+        else if(i % 4 == 0){
+            console.log("Four");
+        }
+        else if(i % 10 == 0){
+            console.log("Ten");
+        }
+        else {
+            console.log(i);
+        }
+    }
 }
